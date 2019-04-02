d1591edac48d8e254e58404c9583beceecc3f2c7
YSI <ysi@adorsys.de>
2019-04-02 23:42:51 +0200

    Mock installer test

--- tests/test_installer.py
+++ tests/test_installer.py
@@ -1,6 +1,11 @@
 from pz.installer import Installer
 
+import mock
 
-def test_update_brew():
+
+@mock.patch('pz.installer.run')
+def test_update_brew(mock_run):
     inst = Installer()
     inst.update()
+    mock_run.assert_called_with(['brew', 'update'])
+
